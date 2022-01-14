8bfb1cede51473a91a19a419709991a4f74a86fb
Jase Seeley <jcseeley31590@yahoo.com>
2022-01-14 13:11:59 -0800

    pair celebrities with language outputs.

--- js/scripts.js
+++ js/scripts.js
@@ -10,24 +10,70 @@ $(document).ready(function() {
 
     $("#hide-quiz").hide();
 
-    if (beverage === "Coffee") {
+    if ((beverage === "Coffee") && (celeb === "Snoop")) {
       $("#java").slideDown();
+      $("#snoop").slideDown();
       $("#reset").slideDown();
-    } else if (beverage === "Red Bull") {
+    } else if ((beverage === "Coffee") && (celeb === "Matt")) {
+      $("#java").slideDown();
+      $("#matthew").slideDown();
+      $("#reset").slideDown();
+    } else if ((beverage === "Coffee") && (celeb === "LeBron")) {
+      $("#java").slideDown();
+      $("#lebron").slideDown();
+      $("#reset").slideDown();
+    } else if ((beverage === "Coffee") && (celeb === "John")) {
+      $("#java").slideDown();
+      $("#john").slideDown();
+      $("#reset").slideDown();
+    } else if ((beverage === "Red Bull") && (celeb === "Snoop")) {
       $("#ruby").slideDown();
+      $("#snoop").slideDown();
       $("#reset").slideDown();
-    } else if (beverage === "Beer") {
+    } else if ((beverage === "Red Bull") && (celeb === "Matt")) {
+      $("#ruby").slideDown();
+      $("#matthew").slideDown();
+      $("#reset").slideDown();
+    } else if ((beverage === "Red Bull") && (celeb === "LeBron")) {
+      $("#ruby").slideDown();
+      $("#lebron").slideDown();
+      $("#reset").slideDown();
+    } else if ((beverage === "Red Bull") && (celeb === "John")) {
+      $("#ruby").slideDown();
+      $("#john").slideDown();
+      $("#reset").slideDown();
+    } else if ((beverage === "Beer") && (celeb === "Snoop")) {
       $("#python").slideDown();
+      $("#snoop").slideDown();
+      $("#reset").slideDown();
+    } else if ((beverage === "Beer") && (celeb === "Matt")) {
+      $("#python").slideDown();
+      $("#matthew").slideDown();
+      $("#reset").slideDown();
+    } else if ((beverage === "Beer") && (celeb === "LeBron")) {
+      $("#python").slideDown();
+      $("#lebron").slideDown();
+      $("#reset").slideDown();
+    } else if ((beverage === "Beer") && (celeb === "John")) {
+      $("#python").slideDown();
+      $("#john").slideDown();
       $("#reset").slideDown();
     } else {
       $("#invalid").slideDown();
+      $("#reset").slideDown();
     }
+    
     $("#reset-quiz").click(function() {
       $("#hide-quiz").slideDown();
       $("#reset").fadeOut();
+      $("#invalid").fadeOut();
       $("#ruby").fadeOut();
       $("#java").fadeOut();
       $("#python").fadeOut();
+      $("#snoop").fadeOut();
+      $("#matthew").fadeOut();
+      $("#lebron").fadeOut();
+      $("#john").fadeOut();
     });
     });
   });
