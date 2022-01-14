75e6d8bf74e9ad1a811e16e0539e5bfab7e4082e
Jase Seeley <jcseeley31590@yahoo.com>
2022-01-14 14:34:55 -0800

    add animal input for first two celebs with coffee.

--- js/scripts.js
+++ js/scripts.js
@@ -10,13 +10,45 @@ $(document).ready(function() {
 
     $("#hide-quiz").hide();
 
-    if ((beverage === "Coffee") && (celeb === "Snoop")) {
+    if ((beverage === "Coffee") && (celeb === "Snoop") && (animal === "coon")) {
       $("#java").slideDown();
       $("#snoop").slideDown();
+      $("#racc").slideDown();
       $("#reset").slideDown();
-    } else if ((beverage === "Coffee") && (celeb === "Matt")) {
+    } else if ((beverage === "Coffee") && (celeb === "Snoop") && (animal === "bun")) {
+      $("#java").slideDown();
+      $("#snoop").slideDown();
+      $("#bunn").slideDown();
+      $("#reset").slideDown();
+    } else if ((beverage === "Coffee") && (celeb === "Snoop") && (animal === "squirrel")) {
+      $("#java").slideDown();
+      $("#snoop").slideDown();
+      $("#squir").slideDown();
+      $("#reset").slideDown();
+    } else if ((beverage === "Coffee") && (celeb === "Snoop") && (animal === "gorilla")) {
+      $("#java").slideDown();
+      $("#snoop").slideDown();
+      $("#gorill").slideDown();
+      $("#reset").slideDown();
+    } else if ((beverage === "Red Bull") && (celeb === "Matt") && (animal === "coon")) {
       $("#java").slideDown();
       $("#matthew").slideDown();
+      $("#racc").slideDown();
+      $("#reset").slideDown();
+    } else if ((beverage === "Coffee") && (celeb === "Matt") && (animal === "bun")) {
+      $("#java").slideDown();
+      $("#matthew").slideDown();
+      $("#bunn").slideDown();
+      $("#reset").slideDown();
+    } else if ((beverage === "Coffee") && (celeb === "Matt") && (animal === "squirrel")) {
+      $("#java").slideDown();
+      $("#matthew").slideDown();
+      $("#squir").slideDown();
+      $("#reset").slideDown();
+    } else if ((beverage === "Coffee") && (celeb === "Matt") && (animal === "gorilla")) {
+      $("#java").slideDown();
+      $("#matthew").slideDown();
+      $("#gorill").slideDown();
       $("#reset").slideDown();
     } else if ((beverage === "Coffee") && (celeb === "LeBron")) {
       $("#java").slideDown();
@@ -74,6 +106,10 @@ $(document).ready(function() {
       $("#matthew").fadeOut();
       $("#lebron").fadeOut();
       $("#john").fadeOut();
+      $("#racc").fadeOut();
+      $("#bunn").fadeOut();
+      $("#squir").fadeOut();
+      $("#gorill").fadeOut();
     });
     });
   });
